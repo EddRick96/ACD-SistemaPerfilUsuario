dc81b71c5c5b9f497b9e3e61ff14f11c1fdaef23
EddRick96 <erickdbg96@gmail.com>
2026-05-06 23:28:54 -0500

    avances bootstrap

---
+++ src/signup.php
@@ -0,0 +1,33 @@
+<?php
+
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Erick Corp - Registro</title>
+    <!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">-->
+</head>
+
+<body>
+    <a aria-current="page" href="../index.php">Inicio</a>
+    <form>
+        <label for="dni">Cédula:</label><br>
+        <input type="text" id="dni" name="dni"><br>
+        <label for="name">Nombre:</label><br>
+        <input type="text" id="name" name="name"><br>
+        <label for="mail">Correo:</label><br>
+        <input type="text" id="mail" name="mail"><br>
+        <label for="pwd">Contraseña:</label><br>
+        <input type="password" id="pwd" name="pwd"><br>
+        <label for="pwd">Repetir contraseña:</label><br>
+        <input type="password" id="pwd" name="pwd"><br>
+        <input type="submit" value="Registro">
+    </form>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script> -->
+</body>
+
+</html>
